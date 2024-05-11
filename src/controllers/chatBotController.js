d9e53b1ca4e6b97b6cb92e8ac6b716debc97bc33
KVK Siddartha <sidduultralegend@gmail.com>
2024-05-12 04:58:04 +0530

    lesgo

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -65,7 +65,7 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {
     // Create the payload for a basic text message
     response = {
-      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+      text: `You sent the message: "${received_message.text}". UwU`,
     };
   } else if (received_message.attachments) {
     // Gets the URL of the message attachment
